a0c0f1fe2fc5069f9a755e5721557de62eafb9f4
pancho <francisco.barriosr@gmail.com>
2018-01-30 11:24:42 -0300

    correccion de foto

--- slider.php
+++ slider.php
@@ -1,2 +1,2 @@
-<img src="http://concursos.beneficioslaaraucana.cl/adminPhotos/assets/header.jpg" class="slide-desktop" style="width: 100%; border-bottom:10px solid #ff9203;">
-<img src="http://concursos.beneficioslaaraucana.cl/adminPhotos/assets/header-mobile.jpg" class="slide-mobile" style="width: 100%; border-bottom:10px solid #ff9203;">
+<img src="assets/header.jpg" class="slide-desktop" style="width: 100%; border-bottom:10px solid #ff9203;">
+<img src="assets/header-mobile.jpg" class="slide-mobile" style="width: 100%; border-bottom:10px solid #ff9203;">
